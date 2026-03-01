4816a706c344317eda4dc33d6c367ec3b4eb50a4
DMITRY-PC\Дмитрий <_andrea_@inbox.ru>
2026-03-02 00:02:01 +0300

    Url Server. Iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,73 @@
 package main
 
-func main() {}
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"net/http"
+)
+
+var dict = make(map[string]string)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/{id}`, getHandler)
+	mux.HandleFunc(`/`, postHandler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "Only GET method", http.StatusBadRequest)
+		return
+	}
+
+	shortUrl := req.URL.Path[1:]
+
+	if url, ok := dict[shortUrl]; ok {
+		w.Header().Add("Location", url)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	} else {
+		http.Error(w, "Url not found!", http.StatusBadRequest)
+	}
+}
+
+func postHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "Only POST method", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, "Invalid body", http.StatusBadRequest)
+		return
+	}
+
+	url := string(body)
+	shortUrl, err := generateShortUrl()
+	if err != nil {
+		http.Error(w, "Generate url failed", http.StatusInternalServerError)
+		return
+	}
+
+	dict[shortUrl] = url
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("http://" + req.Host + "/" + shortUrl))
+}
+
+func generateShortUrl() (string, error) {
+	bytes := make([]byte, 6)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(bytes)[:8], nil
+}
