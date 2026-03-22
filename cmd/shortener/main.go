3ccb3de61f7a6718cf7b1a123a0946b5cbd7ff16
DMITRY-PC\Дмитрий <_andrea_@inbox.ru>
2026-03-22 21:00:00 +0300

    Environment variables

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 
+	"github.com/caarlos0/env/v6"
+
 	"github.com/andrea20024/go-musthave-shortener-tpl/internal/config"
 	"github.com/andrea20024/go-musthave-shortener-tpl/internal/server"
 )
@@ -13,6 +15,7 @@ func main() {
 	flag.StringVar(&config.Host, "a", config.Host, "host")
 	flag.StringVar(&config.BaseURL, "b", config.BaseURL, "base url")
 	flag.Parse()
+	env.Parse(config)
 
 	server.Start(config.Host, config.BaseURL)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.13
 require github.com/stretchr/testify v1.11.1
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.2.5
 	github.com/pmezard/go-difflib v1.0.0 // indirect
