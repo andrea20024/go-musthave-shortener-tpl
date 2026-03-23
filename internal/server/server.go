9f9a0019d532cca518b0041cf5c2788baf5bbbb0
DMITRY-PC\Дмитрий <_andrea_@inbox.ru>
2026-03-23 22:57:33 +0300

    Logger

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,11 +5,26 @@ import (
 	"net/http"
 
 	handlers "github.com/andrea20024/go-musthave-shortener-tpl/internal/handler"
+	logger "github.com/andrea20024/go-musthave-shortener-tpl/internal/logger"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 func Start(host string, baseURL string) {
 	r := chi.NewRouter()
+
+	logg, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logg.Sync()
+
+	logger.InitLogger(logg)
+
+	sugar := *logg.Sugar()
+	sugar.Infow("Starting server", "addr", host)
+	r.Use(logger.WithLogging)
+
 	r.Get("/{id}", handlers.GetHandler)
 	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
 		handlers.PostHandler(w, r, baseURL)
